af105b9bd71cbc84e74295506d6e4025cb0a13be
ThomasMeulendijks <t.meulendijks@outlook.com>
2017-06-15 22:43:49 +0200

    added int suport for values

--- Univeral_Serial_messanger.ino
+++ Univeral_Serial_messanger.ino
@@ -1,13 +1,22 @@
-bool collectingMessage = false;
-bool collectingValue = false;
-String serialMessage = "";
+// two bools that track what we are collecting
+bool collectingMessage;
+bool collectingValue;
+
+String serialMessage; // often used as a command
+String serialValue[serialValueSize]; // often used as params of the command (can contain ints(short I think))
+
 const int serialValueSize = 10; // Pleas ajust the leaght of the array to suit your needs
-String serialValue[serialValueSize]; 
-int currentSerialValueIndex = 0;
+
+bool valueIsInt[serialValueSize]; // used to trak if a value is a int
+
+int currentSerialValueIndex; // the index of the value we are currently collecting
+
 
 void setup() {
   Serial.begin(115200);
-  
+
+  //sets all the values
+  resetVars();
 
 }
 
@@ -16,3 +25,5 @@ void loop() {
 }
 
 
+// Foot note, I kept all the code in two tabs to make it easly exportable and inportable.
+// If you are using this in a bigger project I whould advice to make a "SerialGetter" , "SerialChecker" and a tab whre you use the values.
--- get_n_check.ino
+++ get_n_check.ino
@@ -11,24 +11,20 @@ void checkRemote()
       collectingValue = false;
 
     }
+    // : is used to indicate the start of a new value
     else if (incomingChar == ':')
     {
       // starts reading a new value
-      if (collectingValue == true) {
-        if (currentSerialValueIndex >= (serialValueSize - 1)) {
-          Serial.println("Error: too many values, pleas increas the array size of serialValue ");
-          Serial.println("all other values will be ignored");
-          endMessage();
-        }
-        else {
-          currentSerialValueIndex += 1;
-        }
+      if (currentSerialValueIndex >= (serialValueSize - 1)) {
+        Serial.println("Error: too many values, pleas increas the array size of serialValue ");
+        Serial.println("all other values will be ignored");
+        endMessage();
       }
       else {
-        collectingMessage = false;
-        collectingValue = true;
+        currentSerialValueIndex += 1;
       }
-
+      collectingMessage = false;
+      collectingValue = true;
     }
 
     // % is used to indicate the end of a message
@@ -36,46 +32,71 @@ void checkRemote()
     {
       endMessage();
     }
-
-    else
-    {
-      // If # is recieved, start reading message
-      if (collectingMessage == true && collectingValue == false)
-      {
-        serialMessage = serialMessage + incomingChar;
-      }
-      else if (collectingValue == true && collectingMessage == false) {
-        serialValue[currentSerialValueIndex] = serialValue[currentSerialValueIndex] + incomingChar;
-      }
-      //      else {
-      //        Serial.println("Error, invalid massage");
-      //      }
+    // If # is recieved, start reading message
+    else if (collectingMessage == true && collectingValue == false) {
+      serialMessage = serialMessage + incomingChar;
     }
+    else if (collectingValue == true && collectingMessage == false) {
+      // if one of the incoming chars is not an digit the hole value is not an int
+      if (!isdigit(incomingChar) && valueIsInt[currentSerialValueIndex] == true) {
+        valueIsInt[currentSerialValueIndex] = false;
+      }
+      serialValue[currentSerialValueIndex] = serialValue[currentSerialValueIndex] + incomingChar;
+    }
+    //      else {
+    //        Serial.println("Error, invalid massage");
+    //      }
   }
 }
+
 char getChar()
 {
   int readByte = Serial.read();
   char readChar = (char)readByte;
   return readChar;
 }
+
 void  endMessage() {
-
   // Here you can use the message
-  Serial.println(serialMessage);
-  for (int currIndex = 0; currIndex < (currentSerialValueIndex + 1); ++currIndex) {
-    Serial.println(serialValue[currIndex]);
-  }
-  
+  printMessage();
   delay(100); // small delay to reduce glitch/bug chance can be ajusted to your needs
-
   // Reset the vars
+  resetVars();
+}
+
+void resetVars() {
+  // sets all the indexes to true (messages are an int until proven not to be)
+  for (int currIndex = 0; currIndex < (serialValueSize + 1); ++currIndex) {
+    valueIsInt[currIndex] = true;
+  }
+  // resets all the values
+  for (int currIndex = 0; currIndex < (currentSerialValueIndex + 1); ++currIndex) {
+    serialValue[currIndex] = "";
+  }
   collectingMessage = false;
   collectingValue = false;
   serialMessage = "";
-  for (int currIndex = 0; currIndex < (currentSerialValueIndex + 1); ++currIndex) {
-    serialValue[currIndex] = "";;
-  }
-  currentSerialValueIndex = 0;
+  currentSerialValueIndex = -1;
 }
 
+
+
+
+
+
+// uese values
+
+void printMessage() {
+  //prints the serialMessage
+  Serial.println(serialMessage);
+  // loops all used values and checks if its marked as an int, if so print it as an int, ofcourse you can use this for more usefull stuff.
+  for (int currIndex = 0; currIndex < (currentSerialValueIndex + 1); ++currIndex) {
+    if (valueIsInt[currIndex]) {
+      Serial.print(serialValue[currIndex].toInt());
+      Serial.println("  is a int!");
+    }
+    else {
+      Serial.println(serialValue[currIndex]);
+    }
+  }
+}
